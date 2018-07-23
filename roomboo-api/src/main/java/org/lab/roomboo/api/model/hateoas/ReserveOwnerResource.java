701c3fbc096259eadc930b2a648be929cc6a1091
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-07-23 16:57:32 +0100

    Updated hateoas controllers

--- roomboo-api/src/main/java/org/lab/roomboo/api/model/hateoas/ReserveOwnerResource.java
+++ roomboo-api/src/main/java/org/lab/roomboo/api/model/hateoas/ReserveOwnerResource.java
@@ -3,6 +3,8 @@ package org.lab.roomboo.api.model.hateoas;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import org.apache.commons.lang3.StringUtils;
+import org.lab.roomboo.api.controller.ReserveController;
 import org.lab.roomboo.api.controller.ReserveOwnerController;
 import org.lab.roomboo.domain.model.ReserveOwner;
 import org.springframework.hateoas.ResourceSupport;
@@ -21,8 +23,9 @@ public class ReserveOwnerResource extends ResourceSupport {
 	public ReserveOwnerResource(ReserveOwner reserveOwner) {
 		this.reserveOwner = reserveOwner;
 		String id = reserveOwner.getId();
-		add(linkTo(ReserveOwnerController.class).withRel("owners"));
 		add(linkTo(methodOn(ReserveOwnerController.class).findById(id)).withSelfRel());
+		add(linkTo(ReserveOwnerController.class).withRel("owners"));
+		add(linkTo(methodOn(ReserveController.class).find(StringUtils.EMPTY, id, 0, 10)).withRel("reserves"));
 	}
 
 }
--- roomboo-api/src/main/java/org/lab/roomboo/api/model/hateoas/ReserveResource.java
+++ roomboo-api/src/main/java/org/lab/roomboo/api/model/hateoas/ReserveResource.java
@@ -4,6 +4,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import org.lab.roomboo.api.controller.BuildingController;
+import org.lab.roomboo.api.controller.ReserveController;
 import org.lab.roomboo.api.controller.ReserveOwnerController;
 import org.lab.roomboo.api.controller.RoomController;
 import org.lab.roomboo.domain.model.Reserve;
@@ -21,19 +22,23 @@ public class ReserveResource extends ResourceSupport {
 	public ReserveResource(Reserve reserve) {
 		this.reserve = reserve;
 		String id = reserve.getId();
-		add(linkTo(methodOn(BuildingController.class).findById(id)).withSelfRel());
+		add(linkTo(methodOn(ReserveController.class).findById(id)).withSelfRel());
 
-		if (reserve.getRoom() != null) {
-			String roomId = reserve.getRoom().getId();
-			add(linkTo(methodOn(RoomController.class).findById(roomId)).withRel("room"));
-			reserve.setRoom(Room.builder().id(roomId).build());
-		}
 		if (reserve.getOwner() != null) {
 			String ownerId = reserve.getOwner().getId();
 			add(linkTo(methodOn(ReserveOwnerController.class).findById(ownerId)).withRel("owner"));
 			reserve.setOwner(ReserveOwner.builder().id(ownerId).build());
 		}
+		if (reserve.getRoom() != null) {
+			String roomId = reserve.getRoom().getId();
+			add(linkTo(methodOn(RoomController.class).findById(roomId)).withRel("room"));
 
+			if (reserve.getRoom().getBuilding() != null) {
+				String buildingId = reserve.getRoom().getBuilding().getId();
+				add(linkTo(methodOn(BuildingController.class).findById(buildingId)).withRel("building"));
+			}
+			reserve.setRoom(Room.builder().id(roomId).build());
+		}
 	}
 
 }
