df8ac7a39c116baa3535aa03e67189d6f7801465
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-07-25 15:39:43 +0100

    Fix booking validation issues

--- roomboo-api/src/main/java/org/lab/roomboo/api/model/validation/BookingRequestValidator.java
+++ roomboo-api/src/main/java/org/lab/roomboo/api/model/validation/BookingRequestValidator.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.apache.commons.lang3.StringUtils;
 import org.lab.roomboo.api.model.BookingRequest;
 import org.lab.roomboo.domain.repository.ReserveOwnerRepository;
 import org.lab.roomboo.domain.repository.RoomRepository;
@@ -20,29 +21,45 @@ public class BookingRequestValidator implements ConstraintValidator<ValidBooking
 
 	@Override
 	public boolean isValid(BookingRequest value, ConstraintValidatorContext context) {
-		boolean valid = false;
-		if (!roomRepository.findById(value.getRoomId()).isPresent()) {
+		boolean valid = true;
+		if (StringUtils.isBlank(value.getRoomId())) {
+			context.buildConstraintViolationWithTemplate("Required roomId").addConstraintViolation();
+			valid = false;
+		}
+		else if (!roomRepository.findById(value.getRoomId()).isPresent()) {
 			context.buildConstraintViolationWithTemplate("Invalid roomId " + value.getRoomId())
 				.addConstraintViolation();
 			valid = false;
 		}
-		if (!ownerRepository.findById(value.getOwnerId()).isPresent()) {
+		if (StringUtils.isBlank(value.getOwnerId())) {
+			context.buildConstraintViolationWithTemplate("Required ownerId").addConstraintViolation();
+			valid = false;
+		}
+		else if (!ownerRepository.findById(value.getOwnerId()).isPresent()) {
 			context.buildConstraintViolationWithTemplate("Invalid ownerId " + value.getOwnerId())
 				.addConstraintViolation();
 			valid = false;
 		}
-		if (value.getFrom().isAfter(value.getTo())) {
-			context.buildConstraintViolationWithTemplate("Start date can not be after end date")
-				.addConstraintViolation();
+		if (value.getFrom() == null) {
+			context.buildConstraintViolationWithTemplate("Required from").addConstraintViolation();
 			valid = false;
 		}
-		if (value.getFrom().isBefore(LocalDateTime.now())) {
+		else if (value.getFrom().isBefore(LocalDateTime.now())) {
 			context.buildConstraintViolationWithTemplate("Start date must be after the current date")
 				.addConstraintViolation();
 			valid = false;
 		}
-		if (value.getTo().isBefore(LocalDateTime.now())) {
+		if (value.getTo() == null) {
+			context.buildConstraintViolationWithTemplate("Required to").addConstraintViolation();
+			valid = false;
+		}
+		else if (value.getTo().isBefore(LocalDateTime.now())) {
 			context.buildConstraintViolationWithTemplate("End date must be after the current date")
+				.addConstraintViolation();
+			valid = false;
+		}
+		if (value.getFrom() != null && value.getTo() != null && value.getFrom().isAfter(value.getTo())) {
+			context.buildConstraintViolationWithTemplate("Start date can not be after end date")
 				.addConstraintViolation();
 			valid = false;
 		}
