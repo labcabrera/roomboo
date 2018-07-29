90bc89fbb19e56464b2574c8a1041b06a543780c
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-07-29 09:30:03 +0100

    User register entity validation

--- roomboo-core/src/main/java/org/lab/roomboo/core/model/AppUserRegisterRequest.java
+++ roomboo-core/src/main/java/org/lab/roomboo/core/model/AppUserRegisterRequest.java
@@ -3,6 +3,8 @@ package org.lab.roomboo.core.model;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +20,8 @@ public class AppUserRegisterRequest {
 	private String email;
 
 	@NotBlank
+	@Pattern(regexp = "^[\\w,\\. ]+$")
+	@Size(min = 6, max = 32)
 	private String displayName;
 
 }
