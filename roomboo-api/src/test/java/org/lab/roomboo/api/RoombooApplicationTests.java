35fef93d132fb5dcfe77ea4974df68260c6557c4
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-08-02 09:40:32 +0200

    Fix classpath test issues

--- roomboo-api/src/test/java/org/lab/roomboo/api/RoombooApplicationTests.java
+++ roomboo-api/src/test/java/org/lab/roomboo/api/RoombooApplicationTests.java
@@ -1,20 +1,23 @@
 package org.lab.roomboo.api;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.lab.roomboo.api.config.RoombooConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Slf4j
 public class RoombooApplicationTests {
+
+	@Autowired
+	private RoombooConfig config;
 
 	@Test
 	public void contextLoads() {
-		log.debug("Configuration loaded");
+		Assert.assertNotNull(config);
 	}
 
 }
